d537c2e3f50e5349855e876ce2e7bf5fb34130e4
perry19 <fopaarmel@gmail.com>
2022-06-22 19:57:32 +0100

    added function to handle db connection to postgres db

---
+++ pkg/common/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"api/basic-rest-api/pkg/common/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+// Init db connection
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.Book{})
+
+	return db
+}
