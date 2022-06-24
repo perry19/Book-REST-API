aef637475f4ed7fd1720e06e1e89546eb72cfd5d
perry19 <fopaarmel@gmail.com>
2022-06-24 15:40:01 +0100

    Eited controller.go to add application routes

--- pkg/books/controller.go
+++ pkg/books/controller.go
@@ -1,7 +1,26 @@
 package books
 
-import "gorm.io/gorm"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type handler struct {
 	DB *gorm.DB
+}
+
+// RegisterRoutes registers all the routes
+func RegisterRoutes( r *gin.Engine, db *gorm.DB)  {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := r.Group("/api/v1/books")
+	routes.GET("/:id", h.GetBook)
+	routes.GET("/", h.GetBooks)
+	routes.POST("/", h.AddBook)
+	routes.PUT("/:id", h.UpdateBook)
+	routes.DELETE("/:id", h.DeleteBook)
+
+
 }
